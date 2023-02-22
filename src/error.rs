43bde096e03269d9faf52a992d28c0842b47ab3e
LEQUEUX Maxence <maxence.lequeux@infotel.com>
2023-02-22 16:55:28 +0100

    Began working on repository

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,4 @@
+use axum::http::StatusCode;
 use serde_json::Error;
 
 pub struct ProgramError {
@@ -14,4 +15,9 @@ impl From<std::io::Error> for ProgramError {
     fn from(_: std::io::Error) -> Self {
         ProgramError { msg: "Could not open file" }
     }
+}
+
+pub struct RequestError {
+    pub msg: String,
+    pub status: StatusCode
 }
--- src/repository.rs
+++ src/repository.rs
@@ -1,9 +1,27 @@
 use std::sync::RwLock;
 
-use crate::entity::Restaurants;
+use crate::{entity::{Restaurants, GluttonRequest, GluttonResponse}, error::RequestError, picker::{Pick, StandardPicker}};
 
 static RESTAURANTS: RwLock<Option<Restaurants>> = RwLock::new(None);
 
-pub fn get() -> Result<Restaurants, ()> {
-    todo!();
+pub trait Repository {
+    type Picker: Pick;
+
+    fn find_all() -> Result<Restaurants, RequestError>;
+    fn find_and_pick(request: GluttonRequest) -> Result<GluttonResponse, RequestError> {
+        Ok(Self::Picker::pick(Self::find_all()?, request))
+    }
+}
+
+pub struct StandardRepository {}
+
+impl Repository for StandardRepository {
+    type Picker = StandardPicker;
+
+    fn find_all() -> Result<Restaurants, RequestError> {
+        match RESTAURANTS.read() {
+            Ok(_) => todo!(),
+            Err(_) => todo!(),
+        }
+    }
 }
